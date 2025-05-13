90a32339fe7de91d518a5fabc3085da17065aa6b
Sivasai1810 <nsivasai88@gmail.com>
2025-05-13 20:00:09 +0530

    Update card.jsx

--- src/card.jsx
+++ src/card.jsx
@@ -3,8 +3,8 @@ function card(){
     return(
         <div className="card">
         <img classname="card-image " src={profilepic} alt="arey babu this is profile pic"></img>,
-        <h2 className="card-heading">"this is sai new profile picture"</h2>,
-        <p className="card-para"> "anyways the war has been started"</p>
+        <h2 className="card-heading">this is sai new profile picture</h2>,
+        <p className="card-para">anyways the war has been started</p>
 </div>
     )
 
