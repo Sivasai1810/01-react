dd787f6fb5b73f2522912df8b30603b7b4177cd5
Sivasai1810 <nsivasai88@gmail.com>
2025-05-14 19:33:28 +0530

    updated to do file

--- src/todoapp.jsx
+++ src/todoapp.jsx
@@ -1,34 +1,85 @@
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
+function mytodoapp() {
+  const [task, setTask] = useState(['task1', 'task2', 'task3']);
+  const [newTask, setNewtask] = useState('');
+  const handleInputEvent = (event) => {
+    setNewtask(event.target.value);
+  };
+  const handleAddEvent = () => {
+    if (newTask.trim() !== '') {
+      setTask((t) => [...t, newTask]);
+    }
+    setNewtask('');
+  };
+  const handleDeleteEvent = (index) => {
+    setTask(task.filter((_, i) => i !== index));
+  };
+  const handleUpEvent = (index) => {
+    if (index > 0) {
+      const updatedtask = [...task];
+      [updatedtask[index], updatedtask[index - 1]] = [
+        updatedtask[index - 1],
+        updatedtask[index],
+      ];
+      setTask(updatedtask);
+    }
+  };
+  const handleDownEvent = (index) => {
+    if (index !== task.length - 1) {
+      const updatedtask = [...task];
+      [updatedtask[index], updatedtask[index + 1]] = [
+        updatedtask[index + 1],
+        updatedtask[index],
+      ];
+      setTask(updatedtask);
+    }
+  };
 
+  return (
+    <div className="container">
+      <div className="heading part">
+      <h1 className="heading"> TO-DO-APP</h1>
+      <input
+       className="input"
+        type="text"
+        placeholder="ENTER YOUR TASK"
+        onChange={handleInputEvent}
+        value={newTask}
+      />
+      </div>
 
-function todoapp(){
-    const [todo,setTodo]=useState(["task1","task2"])
-    const [newtask,setNewTask]=useState("")
-  
-function handleInputChange(event){
-setNewTask(event.target.value)
+      <button  className="addbutton" onClick={handleAddEvent}> ADD TASK</button>
+      <h2 className="heading tag">List of task</h2>
+      <div className="bodypart">
+       
+        <ul>
+          {task.map((tasks, index) => (
+            <li className="taskarea" key={index}>
+              {tasks.toUpperCase()}{' '}
+              <button
+                key={index}
+                onClick={() => handleDeleteEvent(index)}
+                className="deletebutton"
+              >
+                DELETE TASK
+              </button>{' '}
+              <button onClick={() => handleUpEvent(index)} className="upbutton">
+                👆
+              </button>
+              <button
+                onClick={() => handleDownEvent(index)}
+                className="downbutton"
+              >
+                👇
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-function handleAddEvent(){
-  if(newtask.trim()!==""){
-    setTodo(t=>[...t,newtask])
-    setNewTask("")
-  }
-}
 
-return(
- <div>
-  <h1>TO-DO-APPLICATION</h1>
-  <div>
-    <h1> LIST OF TASK</h1>
-    <input type="text" placeholder="enter your task" id="taskinput" onChange={handleInputChange}/>
-    <button onClick={handleAddEvent}>ADD TASK</button>
-  </div>
-  <ul>
-    {todo.map((element,index)=><li key={index}>{element}</li>)}
-  </ul>
- </div>
-
-)
-}
-export  default todoapp
+export default mytodoapp;
 
