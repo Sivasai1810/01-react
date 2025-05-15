d4575c5934c5d70e2b803b9ba1583d10dbc0fb9d
Sivasai1810 <nsivasai88@gmail.com>
2025-05-15 11:49:13 +0530

    use

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Todoapp from "./todoapp.jsx"
+import Effect from './useeffect.jsx'
 import  "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<Todoapp />
+    <Effect />
   </StrictMode>
 
   )
